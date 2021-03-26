adc84488555dccbc90af199202007b6faa38b581
Andrew <andrewleigh6@gmail.com>
2021-03-26 14:59:02 +0000

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <React.Fragment>
+    <SEO title="Home" />
+  </React.Fragment>
+)
+
+export default IndexPage

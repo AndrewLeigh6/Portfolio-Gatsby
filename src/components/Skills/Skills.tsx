bf1874596ffd6ff0a1492252421a32eca3dbe774
Andrew <andrewleigh6@gmail.com>
2021-04-03 14:41:46 +0100

    Add additional skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -9,15 +9,36 @@ const Skills = () => {
       <div className="flex container mx-auto justify-around items-center flex-col pl-5 py-10 sm:pl-0 sm:flex-row font-body">
         <List
           title="Front End"
-          skills={["HTML", "CSS", "SCSS / SASS", "JSX", "Bootstrap"]}
+          skills={[
+            "HTML",
+            "CSS",
+            "SCSS / SASS",
+            "Tailwind CSS",
+            "BEM",
+            "Bootstrap",
+          ]}
         />
         <List
           title="Javascript"
-          skills={["React", "Typescript", "Jest", "NodeJS", "Express"]}
+          skills={[
+            "React",
+            "Typescript",
+            "Jest",
+            "NodeJS",
+            "Express",
+            "jQuery",
+          ]}
         />
         <List
           title="Other Tools"
-          skills={["Docker", "Nginx", "MySQL / PostgreSQL", "Vim", "Figma"]}
+          skills={[
+            "Docker",
+            "Nginx",
+            "MySQL / PostgreSQL",
+            "Vim",
+            "Git",
+            "Figma",
+          ]}
         />
       </div>
     </div>
